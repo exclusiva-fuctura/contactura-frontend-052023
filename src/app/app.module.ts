ec00e1a63cfd4a57c8a29cc6da01ad2f976ee051
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-20 04:12:23 -0300

    feat(app): retirar menu improvisado e importar modulos de material e shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material/material.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -20,7 +23,9 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

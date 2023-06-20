66cce75325691e6ae47331c5908e3104454fec21
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-20 04:14:40 -0300

    feat(cadastro): implementar tela

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class CadastroComponent {
 
+  constructor(private router: Router) { }
+
+  public navegarParaRota(rota: string): void {
+    this.router.navigate([rota]);
+  }
 }

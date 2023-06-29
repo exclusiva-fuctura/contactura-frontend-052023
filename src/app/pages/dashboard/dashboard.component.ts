82170100c23f976dad8b62457b2028b82ddca8cc
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-29 03:39:22 -0300

    feat(dashboard): implementar base da tela puxando as receitas do ReceitasService

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,13 +10,15 @@ import { ReceitasService } from 'src/app/shared/services/receitas.service';
 })
 export class DashboardComponent {
 
-  public listaDeReceitas: Receita[] = [];
+  public dataSourceDespesas: any[] = [];
+  public dataSourceReceitas: Receita[] = [];
+  public displayedColumns = ['data','valor','tipo','fixo','descricao','acoes'];
 
   constructor(
     private receitasService: ReceitasService,
     private lancamentosService: LancamentosService
   ) {
-    this.listaDeReceitas = receitasService.getReceitas();
+    this.dataSourceReceitas = receitasService.getReceitas();
 
     this.lancamentosService.getLancamentos();
   }
@@ -30,6 +32,22 @@ export class DashboardComponent {
       data: new Date(),
       valor: 250
     })
+  }
+
+  public onDeleteReceita(item: unknown): void {
+    //
+  }
+
+  public onEditReceita(item: unknown): void {
+    //
+  }
+
+  public onDeleteDespesa(item: unknown): void {
+    //
+  }
+
+  public onEditDespesa(item: unknown): void {
+    //
   }
 
 }

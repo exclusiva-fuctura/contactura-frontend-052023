2966566fb21646cab14a0aec87e0c68689a223c1
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-29 03:41:09 -0300

    refactor(lancamentos): melhorar servico que busca dados dos lancamentos

--- src/app/shared/dao/services/lancamentos.service.ts
+++ src/app/shared/dao/services/lancamentos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { DaoService } from '../dao.service';
 import { Lancamento } from '../../models/lancamento';
+import Swal from 'sweetalert2';
+import { HttpStatusCode } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +15,15 @@ export class LancamentosService {
     this.daoService.get<Array<Lancamento>>(
       "/api/lancamento",
       DaoService.MEDIA_TYPE_APP_JSON
-    ).subscribe((httpResponseLancamento) => {
-      console.log(httpResponseLancamento)
-
-      if (httpResponseLancamento.status === 200) {
-        console.log(httpResponseLancamento.body?.filter)
+    ).subscribe({
+      next: (response) => {
+        if (response.status === HttpStatusCode.Ok) {
+          console.log(response.body)
+        }
+      },
+      error: (err) => {
+        Swal.fire('ALERTA!', 'Ocorreu um erro ao buscar os dados de lançamentos', 'warning');
       }
     });
-
-
   }
 }

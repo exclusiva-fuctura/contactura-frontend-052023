5aec7e2557e5393a80cf63ec70cd5fe2d71f5fdd
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-29 04:18:33 -0300

    feat(lancamentos, receitas): implementar base da tela

--- src/app/lancamentos/pages/receitas/receitas.component.ts
+++ src/app/lancamentos/pages/receitas/receitas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lancamentos-receitas',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class ReceitasComponent {
 
+  public lancamentoReceitaForm: FormGroup;
+  public buttonLabel: string = "Salvar";
+
+  constructor(private formBuilder: FormBuilder) {
+    this.lancamentoReceitaForm = this.formBuilder.group({
+      tipo: ['', Validators.required],
+      ehFixo: false,
+      data: moment().format(),
+      descricao: ['', Validators.required],
+      valor: ['', Validators.required]
+
+    });
+  }
+
+  public get tipos(): string[] {
+    return ['Salario','Extra','Doação','Emprestimo','Investimento'];
+  }
+
+  public onSubmit(): void {
+    //
+  }
+
+  public onReset(): void {
+    this.lancamentoReceitaForm.reset();
+    this.lancamentoReceitaForm.patchValue({
+      data: moment().format(),
+      ehFixo: false
+    });
+  }
+
+  public isFormularioInvalido(): boolean {
+    return this.lancamentoReceitaForm.invalid;
+  }
 }

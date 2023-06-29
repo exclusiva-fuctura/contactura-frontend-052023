2f310d971400d9d6c6c4d94fbc01c2b599be6e66
Thiago Soares <thiagosb.moura@gmail.com>
2023-06-29 04:18:21 -0300

    feat(lancamentos, despesas): implementar base da tela

--- src/app/lancamentos/pages/despesas/despesas.component.ts
+++ src/app/lancamentos/pages/despesas/despesas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lancamentos-despesas',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class DespesasComponent {
 
+  public lancamentoDespesaForm: FormGroup;
+  public buttonLabel: string = "Salvar";
+
+  constructor(private formBuilder: FormBuilder) {
+    this.lancamentoDespesaForm = this.formBuilder.group({
+      tipo: ['', Validators.required],
+      ehFixo: false,
+      data: moment().format(),
+      descricao: ['', Validators.required],
+      valor: ['', Validators.required]
+
+    });
+  }
+
+  public get tipos(): string[] {
+    return ['Alimentação','Habitação','Transporte','Educação','Lazer','Viagem'];
+  }
+
+  public onSubmit(): void {
+    //
+  }
+
+  public onReset(): void {
+    this.lancamentoDespesaForm.reset();
+    this.lancamentoDespesaForm.patchValue({
+      data: moment().format(),
+      ehFixo: false
+    });
+  }
+
+  public isFormularioInvalido(): boolean {
+    return this.lancamentoDespesaForm.invalid;
+  }
 }
